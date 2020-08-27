f75160885be47fc1edf40bf78701c41b0b82a002
Bennar <benja.lazar@gmail.com>
2020-08-27 12:20:57 +0200

    small adjust to layerdim and ignoring batch outputs

--- BiGAN/layerdim_printer.py
+++ BiGAN/layerdim_printer.py
@@ -7,20 +7,22 @@ Created on Fri Apr 10 14:13:18 2020
 
 import numpy as np
 
-def PrintLayerDim(dim_in, params):
-    print('input: ({},{})'.format(dim_in[0],dim_in[1]),
-          'filter: ({},{})'.format(params[0], params[1]),
-          'stride: {}'.format(params[2]),
-          'padding: {}'.format(params[3])
-          )
-
-def ConvTransCalc(dim_in, num_layers, params, dim_out):
-    PrintLayerDim(dim_in, params[0])
-    H = dim_in[0]
-    W = dim_in[1]
-    for i in range(num_layers-1):
-        H = (H - 1) * params[i][2] - 2*params[i][3] + params[i][0] + params[i+1][3]
-        W = (W - 1) * params[i][2] - 2*params[i][3] + params[i][1] + params[i+1][3]
+# =============================================================================
+# def PrintLayerDim(dim_in, params):
+#     print('input: ({},{})'.format(dim_in[0],dim_in[1]),
+#           'filter: ({},{})'.format(params[0], params[1]),
+#           'stride: {}'.format(params[2]),
+#           'padding: {}'.format(params[3])
+#           )
+# 
+# def ConvTransCalc(dim_in, num_layers, params, dim_out):
+#     PrintLayerDim(dim_in, params[0])
+#     H = dim_in[0]
+#     W = dim_in[1]
+#     for i in range(num_layers-1):
+#         H = (H - 1) * params[i][2] - 2*params[i][3] + params[i][0] + params[i+1][3]
+#         W = (W - 1) * params[i][2] - 2*params[i][3] + params[i][1] + params[i+1][3]
+# =============================================================================
 
 def layerdimprint(dim_in, kernel, stride, padding):
     H_out = (dim_in[0] + 2*padding - 1 * (kernel - 1) - 1)/stride + 1
@@ -28,6 +30,14 @@ def layerdimprint(dim_in, kernel, stride, padding):
     print('next layer dim: ', H_out, W_out)
     return H_out, W_out
 
+print('for the original 32x32 input model')
+H_out, W_out = layerdimprint([32, 32], 4,2,1)
+H_out, W_out = layerdimprint([H_out, W_out], 4,2,1)
+H_out, W_out = layerdimprint([H_out, W_out], 4,2,1)
+H_out, W_out = layerdimprint([H_out, W_out], 4,1,0)
+H_out, W_out = layerdimprint([H_out, W_out], 1,1,0)
+
+print('for the expanded model:')
 H_out, W_out = layerdimprint([256, 256], 8,4,2)
 H_out, W_out = layerdimprint([H_out, W_out], 8,4,2)
 H_out, W_out = layerdimprint([H_out, W_out], 4,2,1)
