7df68d907fce0dea46ccd49a166546466a504651
Bennar <benja.lazar@gmail.com>
2021-07-15 13:12:49 +0200

    bigan model 64x64

--- MvTEC/bigan_train.py
+++ MvTEC/bigan_train.py
@@ -19,7 +19,7 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.nn import Conv2d, ConvTranspose2d, BatchNorm2d, LeakyReLU, ReLU, Tanh
-from utils import DeterministicConditional, JointCritic, WALI
+from bigan_utils import DeterministicConditional, JointCritic, WALI
 from dataload import MVTecDataset
 
 # =============================================================================
@@ -31,8 +31,8 @@ from dataload import MVTecDataset
 
 # training hyperparameters
 BATCH_SIZE = 16
-NUM_EPOCHS = 1000
-IMAGE_SIZE = 32
+NUM_EPOCHS = 1
+IMAGE_SIZE = 64
 NUM_CHANNELS = 3
 DIM = 128
 NLAT = 100
@@ -50,37 +50,39 @@ FEATURES_GEN = 16
 
 def create_encoder():
   mapping = nn.Sequential(
-    Conv2d(NUM_CHANNELS, DIM, 4, 2, 1, bias=False), BatchNorm2d(DIM), ReLU(inplace=True),
-    Conv2d(DIM, DIM * 2, 4, 2, 1, bias=False), BatchNorm2d(DIM * 2), ReLU(inplace=True),
-    Conv2d(DIM * 2, DIM * 4, 4, 2, 1, bias=False), BatchNorm2d(DIM * 4), ReLU(inplace=True),
-    Conv2d(DIM * 4, DIM * 4, 4, 1, 0, bias=False), BatchNorm2d(DIM * 4), ReLU(inplace=True),
-    Conv2d(DIM * 4, NLAT, 1, 1, 0))
+    Conv2d(NUM_CHANNELS, FEATURES_GEN * 2, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 2), ReLU(),
+    Conv2d(FEATURES_GEN * 2, FEATURES_GEN * 4, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 4), ReLU(),
+    Conv2d(FEATURES_GEN * 4, FEATURES_GEN * 8, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 8), ReLU(),
+    Conv2d(FEATURES_GEN * 8, FEATURES_GEN * 16, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 16), ReLU(),
+    Conv2d(FEATURES_GEN * 16, NLAT, 4, 1, 0))
   return DeterministicConditional(mapping)
 
 
 def create_generator():
   mapping = nn.Sequential(
-    ConvTranspose2d(NLAT, FEATURES_GEN * 16, 4, 1, 0, bias=False), BatchNorm2d(FEATURES_GEN * 16), ReLU(),
-    ConvTranspose2d(FEATURES_GEN * 16, FEATURES_GEN * 8, 4, 2, 1, bias=False), BatchNorm2d(FEATURES_GEN * 8), ReLU(),
-    ConvTranspose2d(FEATURES_GEN * 8, FEATURES_GEN * 4, 4, 2, 1, bias=False), BatchNorm2d(FEATURES_GEN * 4), ReLU(),
-    ConvTranspose2d(FEATURES_GEN * 4, FEATURES_GEN * 2, 4, 2, 1, bias=False), BatchNorm2d(FEATURES_GEN * 2), ReLU(),
-    ConvTranspose2d(FEATURES_GEN * 2, NUM_CHANNELS, 4, 2, 1, bias=False), nn.Tanh())    
+    ConvTranspose2d(NLAT, FEATURES_GEN * 16, 4, 1, 0), BatchNorm2d(FEATURES_GEN * 16), ReLU(),
+    ConvTranspose2d(FEATURES_GEN * 16, FEATURES_GEN * 8, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 8), ReLU(),
+    ConvTranspose2d(FEATURES_GEN * 8, FEATURES_GEN * 4, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 4), ReLU(),
+    ConvTranspose2d(FEATURES_GEN * 4, FEATURES_GEN * 2, 4, 2, 1), BatchNorm2d(FEATURES_GEN * 2), ReLU(),
+    ConvTranspose2d(FEATURES_GEN * 2, NUM_CHANNELS, 4, 2, 1), nn.Tanh())    
   return DeterministicConditional(mapping)
 
 
 def create_critic():
   x_mapping = nn.Sequential(
-    Conv2d(NUM_CHANNELS, DIM, 4, 2, 1), LeakyReLU(LEAK),
-    Conv2d(DIM, DIM * 2, 4, 2, 1), LeakyReLU(LEAK),
-    Conv2d(DIM * 2, DIM * 4, 4, 2, 1), LeakyReLU(LEAK),
-    Conv2d(DIM * 4, DIM * 4, 4, 1, 0), LeakyReLU(LEAK))
+    Conv2d(NUM_CHANNELS, FEATURES_GEN * 2, 4, 2, 1), LeakyReLU(LEAK),
+    Conv2d(FEATURES_GEN * 2, FEATURES_GEN * 4, 4, 2, 1), LeakyReLU(LEAK),
+    Conv2d(FEATURES_GEN * 4, FEATURES_GEN * 8, 4, 2, 1), LeakyReLU(LEAK),
+    Conv2d(FEATURES_GEN * 8, FEATURES_GEN * 16, 4, 2, 1), LeakyReLU(LEAK),
+    Conv2d(FEATURES_GEN * 16, FEATURES_GEN * 16, 4, 1, 0), LeakyReLU(LEAK),
+    )
 
   z_mapping = nn.Sequential(
     Conv2d(NLAT, 512, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(512, 512, 1, 1, 0), LeakyReLU(LEAK))
 
   joint_mapping = nn.Sequential(
-    Conv2d(DIM * 4 + 512, 1024, 1, 1, 0), LeakyReLU(LEAK),
+    Conv2d(FEATURES_GEN * 16 + 512, 1024, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(1024, 1024, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(1024, 1, 1, 1, 0))
 
@@ -106,7 +108,7 @@ optimizerC = Adam(wali.get_critic_parameters(),
 dataset = MVTecDataset(class_name='hazelnut', is_train=True, resize=IMAGE_SIZE)
 loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
+fixed_noise = torch.randn(32, NLAT, 1, 1).to(device)
 writer = SummaryWriter()
 
 curr_iter = C_iter = EG_iter = 0
@@ -157,7 +159,7 @@ for epoch in range(NUM_EPOCHS):
             fake = wali.generate(fixed_noise)
             rect = wali.reconstruct(init_x[:32])
             # take out (up to) 32 examples
-            img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
+            img_grid_real = torchvision.utils.make_grid(x[:32], normalize=True)
             img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
             img_grid_rect = torchvision.utils.make_grid(rect[:32], normalize=True)
             writer.add_image("Real", img_grid_real, global_step=epoch)
